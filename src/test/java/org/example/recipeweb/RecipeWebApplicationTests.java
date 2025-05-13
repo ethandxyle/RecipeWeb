fe9fbe49d84637fdae442b8ebd6ae06494956c63
Ethan Doyle <Ethan.Doyle.mil@usmc.mil>
2025-05-13 10:34:06 -0500

    new repo commit

---
+++ src/test/java/org/example/recipeweb/RecipeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recipeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
